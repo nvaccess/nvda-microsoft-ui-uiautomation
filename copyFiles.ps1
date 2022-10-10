63d752a199b98e752cdb18e7fe1aa8b76e06c7e4
Reef Turner <feerrenrut@users.noreply.github.com>
2022-10-10 18:05:32 +0800

    Copy required files into the output structure.

---
+++ copyFiles.ps1
@@ -0,0 +1,62 @@
+param(
+    [String]$RepoRoot = $(Get-Location),
+    [String]$BuildDir = $(Join-Path $(Get-Location) "out"),
+    [String]$DestDir = $(Join-Path $(Get-Location)"gen")
+)
+##
+# Collects files from the build of the Microsoft-UI-UIAutomation Remote Operations library.
+
+function CopyFileStructure(
+    [String]$OriginRelativeDir,  # Origin files are all relative to this dir.
+    [String]$DestDir,  # Destination relative to this dir.
+    [String[]]$RelativeOriginFiles
+){
+    Write-Output $("From: " + $OriginRelativeDir)
+    Write-Output $("To: " + $DestDir)
+    $RelativeOriginFiles.ForEach({
+        Write-Output $("Copying: " + $_)
+        $itemOrigin = Join-Path $OriginRelativeDir $_
+        $itemDest = Join-Path $DestDir $_
+         # Ensure the dest path exists
+        $null = New-Item -ItemType File -Path $itemDest -Force
+        # Overwrite the blank file
+        $null = Copy-Item $itemOrigin -Destination $itemDest -Force
+    })
+    Write-Output " "
+}
+
+# Populate Lib dir
+$libDir = Join-Path $DestDir "lib"
+CopyFileStructure -OriginRelativeDir $RepoRoot -DestDir $libDir -RelativeOriginFiles @(
+    $("Microsoft.UI.UIAutomation.dll.manifest")
+)
+
+CopyFileStructure -OriginRelativeDir $BuildDir -DestDir $libDir -RelativeOriginFiles @(
+    $("winmd/Microsoft.UI.UIAutomation.winmd"),
+
+    $("Microsoft.UI.UIAutomation.dll"),
+    $("Microsoft.UI.UIAutomation.lib"),
+    $("Microsoft.UI.UIAutomation.exp"),
+    $("Microsoft.UI.UIAutomation.pdb"),
+
+    $("UiaOperationAbstraction.lib"),
+    $("UiaOperationAbstraction.pdb")
+)
+
+$MsUiaSubModuleSrcDir = Join-Path $RepoRoot "microsoft-ui-uiautomation\src\UIAutomation\"
+
+$includesDir = Join-Path $DestDir 'include'
+CopyFileStructure -OriginRelativeDir $MsUiaSubModuleSrcDir -DestDir $includesDir -RelativeOriginFiles @(
+    $('UiaOperationAbstraction/UiaOperationAbstraction.h'),
+    $('UiaOperationAbstraction/UiaTypeAbstractionEnums.g.h'),
+    $('UiaOperationAbstraction/UiaTypeAbstraction.g.h'),
+    $('UiaOperationAbstraction/SafeArrayUtil.h')
+)
+
+$genSrcDir = Join-Path $MsUiaSubModuleSrcDir "microsoft.ui.uiautomation/Generated Files"
+CopyFileStructure -OriginRelativeDir $genSrcDir -DestDir $includesDir -RelativeOriginFiles @(
+    $('winrt/microsoft.ui.uiautomation.h'),
+    $('winrt/impl/microsoft.ui.uiautomation.0.h'),
+    $('winrt/impl/microsoft.ui.uiautomation.1.h'),
+    $('winrt/impl/microsoft.ui.uiautomation.2.h')
+)
--- readme.md
+++ readme.md
@@ -6,6 +6,22 @@ Builds the open source Microsoft-UI-UIAutomation Remote Operations library from
 https://github.com/microsoft/microsoft-ui-uiautomation.
 
 This library contains both a low-level winrt API, and a higher-level pure C++ API.
+Once built, the following outputs are copied and committed to the `main-out` branch.
+
+The outputs:
+* `lib` directory:
+  * `microsoft.ui.uiAutomation[.dll .lib .dll.manifest]`:
+    The shared libraries containing the low-level winrt implementation.
+    To use in other code, link against the `.lib`, and load the included `.manifest` file into
+    an activation context and activate it.
+  * `UiaOperationAbstraction.lib`:
+    A static library containing runtime code for the higher-level pure C++ API.
+    This should be linked into any compiled executable or library that needs to use the
+    higher-level C++ API.
+* `include` directory:
+  * `UIAOperationAbstraction` directory: All public headers for the high-level C++ API.
+  * `winrt` directory: The generated C++/winrt language bindings of the low-level API,
+    required by the high-level C++ API headers.
 
 ### Clone
 Ensure submodules are cloned.
@@ -15,4 +31,5 @@ The `microsoft-ui-uiautomation` repository is a submodule.
 
 Use powershell:
 - `mkdir out` Create an output directory.
-- `build.ps1 -OutDir out`
+- `./build.ps1 -OutDir out`
+- `./copyFiles.ps1 -RepoRoot "./" -BuildDir "out" -DestDir "gen"`  Generate complete structure.
